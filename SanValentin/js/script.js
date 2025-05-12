53b507bc2b93e69054c1f2d807523fbab0d11fc4
axellicoa <134027682+axellicoaa@users.noreply.github.com>
2025-05-12 18:43:48 -0500

    Delete SanValentin directory

--- SanValentin/js/script.js
+++
@@ -1,9 +0,0 @@
-let btnNo = document.getElementById("btnNo");
-btnNo.addEventListener("click", () => { 
-    let currentSize = parseInt(window.getComputedStyle(btnNo).fontSize);
-    if (currentSize > 5) {
-        btnNo.style.fontSize = (currentSize -6) + "px";
-    } else {
-        btnNo.style.display = "none";
-    }
-});
